197c3e836f187f65659c58bb0160b3af0a4b04a5
YoMarguerite <yoann.marguerite@orange.fr>
2023-08-10 15:54:27 +0200

    page motion

--- pages/motion.js
+++ pages/motion.js
@@ -14,9 +14,72 @@ export default function Motion() {
       <main>
         <Header title="Fio fio" />
         <Nav active="Motion"/>
-        <p className="description">
-          Motion
-        </p>
+
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <video width="100%" autoPlay={true} muted loop>
+                <source src="/fiona_motion/Motion/A.mp4" type="video/mp4"/>
+                Error Message
+              </video>
+            </div>
+            <div class="col">
+              <video width="100%" autoPlay={true} muted loop>
+                <source src="/fiona_motion/Motion/B.mp4" type="video/mp4"/>
+                Error Message
+              </video>
+            </div>
+            <div class="col">
+              <video width="100%" autoPlay={true} muted loop>
+                <source src="/fiona_motion/Motion/C.mp4" type="video/mp4"/>
+                Error Message
+              </video>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col">
+              <video width="100%" autoPlay={true} muted loop>
+                <source src="/fiona_motion/Motion/D.mp4" type="video/mp4"/>
+                Error Message
+              </video>
+            </div>
+            <div class="col">
+              <video width="100%" autoPlay={true} muted loop>
+                <source src="/fiona_motion/Motion/E.mp4" type="video/mp4"/>
+                Error Message
+              </video>
+            </div>
+            <div class="col">
+              <video width="100%" autoPlay={true} muted loop>
+                <source src="/fiona_motion/Motion/F.mp4" type="video/mp4"/>
+                Error Message
+              </video>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col">
+              <video width="100%" autoPlay={true} muted loop>
+                <source src="/fiona_motion/Motion/G.mp4" type="video/mp4"/>
+                Error Message
+              </video>
+            </div>
+            <div class="col">
+              <video width="100%" autoPlay={true} muted loop>
+                <source src="/fiona_motion/Motion/H.mp4" type="video/mp4"/>
+                Error Message
+              </video>
+            </div>
+            <div class="col">
+              <video width="100%" autoPlay={true} muted loop>
+                <source src="/fiona_motion/Motion/I.mp4" type="video/mp4"/>
+                Error Message
+              </video>
+            </div>
+          </div>
+        </div>
+        
       </main>
 
       <Footer />
