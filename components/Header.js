434d4df4e772129432cd835a457e02c2b7a783aa
YoMarguerite <yoann.marguerite@orange.fr>
2023-10-18 16:37:26 +0200

    header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 export default function Header({  }) {
   return (
     <div class="container">
-      <video width="15%" class="rounded-circle" autoPlay={true} muted loop controls>
+      <video class="head-img" autoPlay={true} muted loop controls>
         <source src="/fiona_motion/Logo Home/OEUIL_2.mp4" type="video/mp4"/>
         Error Message
       </video>
